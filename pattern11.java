afbe0a2fd17097f8116a1fea08204c89bb4497e9
snehagulagond <snehagulagond@gmail.com>
2026-04-28 20:50:37 +0530

    Add files via upload

---
+++ pattern11.java
@@ -0,0 +1,19 @@
+public class pattern11 {
+    public static void main(String[]args){
+    int n = 5;
+    
+    for(int i=0;i<n;i++){
+        int start=1;
+        if(i%2==0)start=1;
+        else{
+            start=0;
+
+        }
+        for(int j=0;j<=i;j++){
+            System.out.print(start);
+            start=1-start;
+        }
+        System.out.println();
+    }
+    }
+}
