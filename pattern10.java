4cc709355d0c37deaac7d3cb99cf07c60815793b
snehagulagond <snehagulagond@gmail.com>
2026-04-28 20:50:12 +0530

    Add files via upload

---
+++ pattern10.java
@@ -0,0 +1,18 @@
+public class pattern10 {
+    public static void main(String[]args){
+    int n = 5;
+    for(int i=1;i<2*n-1;i++){
+        int start=i;
+            if(i>n)start=2*n-i;
+            
+        for(int j=1;j<=start;j++){
+            
+            System.out.print('*');
+        }
+        System.out.println();
+    }
+}
+    
+}
+
+
