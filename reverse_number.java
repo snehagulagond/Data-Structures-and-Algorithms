9c6e28c4042eeadcadf56e78210f6f1a973dcb3d
snehagulagond <snehagulagond@gmail.com>
2026-05-02 21:37:31 +0530

    Add files via upload

---
+++ reverse_number.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class reverse_number {
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number");
+        int n=sc.nextInt();
+      
+        int reverse=0;
+        int digit;
+        
+        
+        while(n!=0){
+            
+            digit=n%10;
+
+            reverse=reverse*10+digit;
+            n=n/10;
+            
+        }
+       
+        System.out.println(reverse);        
+
+    }
+
+    
+}
