74545606d44740c1f7b5f56343b76ac5b99c6374
snehagulagond <snehagulagond@gmail.com>
2026-05-02 21:37:11 +0530

    Add files via upload

---
+++ pattern15.java
@@ -0,0 +1,15 @@
+public class pattern15 {
+    public static void main(String[]args){
+    int n = 5;
+    
+    for(int i=0;i<=n;i++){
+        char ch = (char) ('A' + i);   // typecasting
+        for(int j=0;j<=i;j++){
+            System.out.print(ch);
+
+        }
+        System.out.println();
+    }
+
+    }
+}
